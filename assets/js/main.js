44ca2e796c959e4ea8afbe38042f30b78e27649c
Kahina Ouali <kahinaouali.pro@gmail.com>
2024-10-24 20:55:47 +0200

    New design for the homepage

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-AOS.init({
-    duration: 1200, // Durée de l'animation en millisecondes
-    easing: 'ease-in-out', // Fonction de transition
-    offset: 400, // Décalage de déclenchement de l'animation
-    once: true, // Activer l'animation uniquement une fois (false pour animer à chaque défilement)
-});
